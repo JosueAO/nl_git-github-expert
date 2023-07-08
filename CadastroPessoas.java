0617d1a742d3f609fd7f2502a61e7c4d2f81220c
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2023-07-08 06:23:58 -0300

    Cdastrar e Listar Pessoas

---
+++ CadastroPessoas.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Pessoa {
+    private String nome;
+    private int idade;
+    
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+}
+
+public class CadastroPessoas {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("=== Cadastro de Pessoas ===");
+            System.out.println("1. Cadastrar pessoa");
+            System.out.println("2. Listar pessoas cadastradas");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            
+            int opcao = scanner.nextInt();
+            
+            if (opcao == 1) {
+                System.out.print("Nome: ");
+                String nome = scanner.next();
+                
+                System.out.print("Idade: ");
+                int idade = scanner.nextInt();
+                
+                Pessoa pessoa = new Pessoa(nome, idade);
+                pessoas.add(pessoa);
+                
+                System.out.println("Pessoa cadastrada com sucesso!\n");
+            } else if (opcao == 2) {
+                if (pessoas.isEmpty()) {
+                    System.out.println("Nenhuma pessoa cadastrada.\n");
+                } else {
+                    System.out.println("=== Pessoas Cadastradas ===");
+                    for (Pessoa pessoa : pessoas) {
+                        System.out.println("Nome: " + pessoa.getNome());
+                        System.out.println("Idade: " + pessoa.getIdade());
+                        System.out.println("-----------------------");
+                    }
+                    System.out.println();
+                }
+            } else if (opcao == 3) {
+                System.out.println("Saindo do programa...");
+                break;
+            } else {
+                System.out.println("Opção inválida. Tente novamente.\n");
+            }
+        }
+        
+        scanner.close();
+    }
+}
