8f580cafe5cf07d0cf5f7d3cf1bce3f8c0b422e6
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2023-07-08 06:45:11 -0300

    Refatorado com a feature de classe record - Java 14+

--- CadastroPessoas.java
+++ CadastroPessoas.java
@@ -2,48 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Pessoa {
-    private String nome;
-    private int idade;
-    
-    public Pessoa(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-    
-    public int getIdade() {
-        return idade;
-    }
-}
+public record Pessoa(String nome, int idade) {}
 
 public class CadastroPessoas {
     public static void main(String[] args) {
         List<Pessoa> pessoas = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             System.out.println("=== Cadastro de Pessoas ===");
             System.out.println("1. Cadastrar pessoa");
             System.out.println("2. Listar pessoas cadastradas");
             System.out.println("3. Sair");
             System.out.print("Escolha uma opção: ");
-            
+
             int opcao = scanner.nextInt();
-            
+
             if (opcao == 1) {
                 System.out.print("Nome: ");
                 String nome = scanner.next();
-                
+
                 System.out.print("Idade: ");
                 int idade = scanner.nextInt();
-                
+
                 Pessoa pessoa = new Pessoa(nome, idade);
                 pessoas.add(pessoa);
-                
+
                 System.out.println("Pessoa cadastrada com sucesso!\n");
             } else if (opcao == 2) {
                 if (pessoas.isEmpty()) {
@@ -51,8 +35,8 @@ public class CadastroPessoas {
                 } else {
                     System.out.println("=== Pessoas Cadastradas ===");
                     for (Pessoa pessoa : pessoas) {
-                        System.out.println("Nome: " + pessoa.getNome());
-                        System.out.println("Idade: " + pessoa.getIdade());
+                        System.out.println("Nome: " + pessoa.nome());
+                        System.out.println("Idade: " + pessoa.idade());
                         System.out.println("-----------------------");
                     }
                     System.out.println();
@@ -64,7 +48,7 @@ public class CadastroPessoas {
                 System.out.println("Opção inválida. Tente novamente.\n");
             }
         }
-        
+
         scanner.close();
     }
 }
